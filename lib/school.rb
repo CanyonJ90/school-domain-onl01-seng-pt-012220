35c693c5415dc1903624e19e062923a1433a310c
Canyon Mathis <canyon.mathis@gmail.com>
2020-02-11 00:16:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,6 +10,17 @@ class School
     roster[level] ||= []
     roster[level] << student
   end
+  
+  
+  def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+  
+end 
 end
 
 
